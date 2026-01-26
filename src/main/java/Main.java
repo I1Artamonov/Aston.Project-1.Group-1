2289ba8110501d219dc42aff6b09bb626ff42d76
dmitrii_riabin <dmitry.ryabin@gmail.com>
2026-01-26 20:26:12 +0300

    add: nwe branch was crated to develop the main method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,35 @@
 public class Main {
+
+//    private static boolean isRunning = true;
+//
+//    Scanner scanner = new Scanner(System.in);
+//
+//while (isRunning) {
+//
+//        showMenu();
+//        String userInput = //принимаем значение со сканнера
+//
+//        if(userInput.equalsIgnorCase(add)) {}
+//        if(userInput.equalsIgnorCase(search)) {}
+//        if(userInput.equalsIgnorCase(list)) {}
+//        if(userInput.equalsIgnorCase(exit)) {
+//            "Программа завершает работу! До новых встреч!"
+//            isRunning = false;
+//        }
+//    }
+//
+//    public static void showMenu() {
+//        add - добавить что-то в формате таком-то
+//        search - найти
+//        list - показать
+//        exit - выход
+//    }
+//
+//    public static void addValidation() {}
+//
+//    public static void searchValidation() {}
+//
+//    public static void listValidation() {}
+
+
 }
